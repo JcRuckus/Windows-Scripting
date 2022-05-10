69179bee96f5aea25729479de7995e12f042f1b6
JcRuckus <105320032+JcRuckus@users.noreply.github.com>
2022-05-10 16:35:56 -0500

    Update fixprinter-DELjobs.bat

--- fixprinter-DELjobs.bat
+++ fixprinter-DELjobs.bat
@@ -1,10 +1,10 @@
 @ECHO OFF
 :START
-runas /user: "%windir%\system32\net stop spooler"
+"%windir%\system32\net stop spooler"
 
-runas /user: del /Q /F /S "%windir%\System32\spool\PRINTERS\*.*"
+del /Q /F /S "%windir%\System32\spool\PRINTERS\*.*"
 
-runas /user: "%windir%\system32\net start spooler"
+"%windir%\system32\net start spooler"
 
 PAUSE
-goto START
+
